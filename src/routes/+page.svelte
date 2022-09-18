e2f20500009d3b43b1888b474ff7169307e78e90
Rodrigo Couto <r@rodg.co>
2022-09-18 19:35:59 -0300

    Update sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import store from '$lib/test/store';
+
+	let value = $store;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<h2 aria-live="polite" role="log">{$store}, Rodrigo.</h2>
+<form on:submit|preventDefault={() => store.greet(value)}>
+	<input type="text" bind:value />
+</form>
