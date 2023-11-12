5ecbd4bdef026f7418620e4ee2f1e16635046714
Rodrigo Couto <r@rodg.co>
2023-11-11 22:48:22 -0300

    Fix name conflict

--- src/lib/util/index.js
+++ src/lib/util/index.js
@@ -5,8 +5,8 @@ export const isBrowser = typeof window !== 'undefined' && typeof window.document
 export const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
 
 /** @type {Storage | null} */
-export const localStorage = isBrowser ? window.localStorage : null;
+export const getLocalStorage = isBrowser ? window.localStorage : null;
 
 /** @type {Storage | null} */
-export const sessionStorage = isBrowser ? window.sessionStorage : null;
+export const getSessionStorage = isBrowser ? window.sessionStorage : null;
 
